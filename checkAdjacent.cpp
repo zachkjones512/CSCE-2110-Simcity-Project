e478956afa9c2a2b8d35504cd7fb2cb2737692cc
Jones, Zachary <zacharyjones4@my.unt.edu>
2022-10-19 14:01:34 -0500

    Initialized adjacent population function

---
+++ checkAdjacent.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+
+#include <"main.h">
+
+int checkAdjacent(node &array){
+    int sum=0;
+
+    if(array[i][j].nodeType = 'r'){
+
+        sum = array[i-1][j-1]+array[i-1][j]+array[i-1][j+1]+array[i][j-1]+array[i][j+1]+array[i+1][j-1]+array[i+1][j]+array[i+1][j+1];
+    }
+
+   return sum;
+
+}
