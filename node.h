76c7daac04bbee5dd15ea13af7404caac08fcd90
Caleb Smay <calebsmay@my.unt.edu>
2022-10-24 12:46:10 -0500

    Added many new files, trying to figure out gitignore files, removed the main function from checkAdjacent.cpp

---
+++ node.h
@@ -0,0 +1,18 @@
+#include <iostream>
+
+class Node{
+    public:
+    //Constructors
+        Node();
+        Node(char NodeType);
+    //Mutators
+    //Accessors
+    //Destructors
+        ~Node();
+    private:
+        char NodeType;
+        int population;
+        int pollution;
+        int workers;
+        int goods
+};
