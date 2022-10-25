71156e8575317b723a77b8dfc384da6f8961a917
Caleb Smay <calebsmay@my.unt.edu>
2022-10-24 20:06:17 -0500

    Added SetType and GetType functions

--- node.cpp
+++ node.cpp
@@ -1,1 +1,30 @@
 #include "node.h"
+
+//Constructors
+Node::Node(){
+    //Defaults to road if no type
+    this->nodeType = 'r';
+    this->population = 0;
+    this->pollution = 0;
+    this->workers = 0;
+    this->goods = 0;
+}
+Node::Node(char nodeType){
+    this->nodeType = nodeType;
+    this->population = 0;
+    this->pollution = 0;
+    this->workers = 0;
+    this->goods = 0;
+}
+//Mutators
+void Node::SetType(char nodeType){
+    this->nodeType = nodeType;
+}
+//Accessors
+char Node::GetType(){
+    return this->nodeType;
+}
+//Destructors
+Node::~Node(){
+    //TODO: ADD STUFF
+}
--- node.h
+++ node.h
@@ -4,15 +4,17 @@ class Node{
     public:
     //Constructors
         Node();
-        Node(char NodeType);
+        Node(char nodeType);
     //Mutators
+        void SetType(char nodeType);
     //Accessors
+        char GetType();
     //Destructors
         ~Node();
     private:
-        char NodeType;
+        char nodeType;
         int population;
         int pollution;
         int workers;
-        int goods
+        int goods;
 };
