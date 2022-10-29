ef1fa815f34949f524ea1e955e02f3b603981800
Jones, Zachary <zacharyjones4@my.unt.edu>
2022-10-28 20:39:33 -0500

    Added mutator and accessor for pop

--- node.cpp
+++ node.cpp
@@ -20,9 +20,15 @@ Node::Node(char nodeType){
 void Node::SetType(char nodeType){
     this->nodeType = nodeType;
 }
+void Node::SetPop(int pop){
+    this->population = pop;
+}
 //Accessors
 char Node::GetType(){
     return this->nodeType;
+}
+int Node::GetPop(){
+    return this->population;
 }
 //Destructors
 Node::~Node(){
