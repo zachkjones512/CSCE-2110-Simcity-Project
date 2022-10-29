03584c280df40b098bff70f162b8af5319d3a57a
Jones, Zachary <zacharyjones4@my.unt.edu>
2022-10-29 08:49:21 -0500

    Fixed compilation error in update function

--- reszone.cpp
+++ reszone.cpp
@@ -8,44 +8,45 @@ void resUpdate(Node*** array1, Node*** array2, int arrayx, int arrayy){
 
         for(int j=0; j<arrayx; ++j){
 
+            if(array1[j][i]->GetType() == 'R'){            
+                int popnum = checkAdjacent(array1, i, j, arrayx, arrayy); //assigns adjacent cell total population to a num
 
-            int popnum = checkAdjacent(array1, i, j, arrayx, arrayy); //assigns adjacent cell total population to a num
+                switch(array1[j][i]->GetPop()){ //switch case to check for update conditions
 
-            switch(array1[j][i]->GetPop()){ //switch case to check for update conditions
+                case 0: //if pop is 0 and there's an adjacent cell with a pop of 1, increase current cell population
+                if (popnum >=1){
+                    array2[j][i]->SetPop(1);
+                }
+                break;
 
-            case 0: //if pop is 0 and there's an adjacent cell with a pop of 1, increase current cell population
-            if (popnum >=1){
-                array2[j][i]->SetPop(1);
+                case 1:
+                if (popnum >=2){
+                    array2[j][i]->SetPop(2);
+                }
+                break;
+
+                case 2:
+                if (popnum >=8){
+                    array2[j][i]->SetPop(3);
+                }
+                break;
+
+                case 3:
+                if (popnum >=18){
+                    array2[j][i]->SetPop(4);
+                }
+                break;
+
+                case 4:
+                if (popnum >=32){
+                    array2[j][i]->SetPop(5);
+                }
+                break;            
+
+                default: //do nothing if population is over 5.
+                break;
+                }
             }
-            break;
-
-            case 1:
-            if (popnum >=2){
-                array2[j][i]->SetPop(2);
-            }
-            break;
-
-            case 2:
-            if (popnum >=8){
-                array2[j][i]->SetPop(3);
-            }
-            break;
-
-            case 3:
-            if (popnum >=18){
-                array2[j][i]->SetPop(4);
-            }
-            break;
-
-            case 4:
-            if (popnum >=32){
-                array2[j][i]->SetPop(5);
-            }
-            break;            
-
-            default: //do nothing if population is over 5.
-            break;
-        }
     }
 
 
