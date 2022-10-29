851940549c443f428f9aa4c347200e8350a3546d
Jones, Zachary <zacharyjones4@my.unt.edu>
2022-10-29 09:04:31 -0500

    Added worker functionality

--- reszone.cpp
+++ reszone.cpp
@@ -1,7 +1,7 @@
 #include "reszone.h"
 #include "checkAdjacent.h"
 
-void resUpdate(Node*** array1, Node*** array2, int arrayx, int arrayy, &workers){
+void resUpdate(Node*** array1, Node*** array2, int arrayx, int arrayy, int &workers){
 
     for(int i=0; i<arrayy; ++i){
 
@@ -16,30 +16,35 @@ void resUpdate(Node*** array1, Node*** array2, int arrayx, int arrayy, &workers)
                 case 0: //if pop is 0 and there's an adjacent cell with a pop of 1, increase current cell population
                 if (popnum >=1){
                     array2[j][i]->SetPop(1);
+                    ++workers;
                 }
                 break;
 
                 case 1:
                 if (popnum >=2){
                     array2[j][i]->SetPop(2);
+                    ++workers;
                 }
                 break;
 
                 case 2:
                 if (popnum >=8){
                     array2[j][i]->SetPop(3);
+                    ++workers;
                 }
                 break;
 
                 case 3:
                 if (popnum >=18){
                     array2[j][i]->SetPop(4);
+                    ++workers;
                 }
                 break;
 
                 case 4:
                 if (popnum >=32){
                     array2[j][i]->SetPop(5);
+                    ++workers;
                 }
                 break;            
 
