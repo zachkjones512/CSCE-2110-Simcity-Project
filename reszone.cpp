865ad38f61a85e0d80d2b448d0923431c9fe1386
Jones, Zachary <zacharyjones4@my.unt.edu>
2022-10-28 20:51:02 -0500

    Fixed compilation errors

--- reszone.cpp
+++ reszone.cpp
@@ -11,35 +11,35 @@ void resUpdate(Node*** array1, Node*** array2, int arrayx, int arrayy){
 
             int popnum = checkAdjacent(array2, i, j, arrayx, arrayy); //assigns adjacent cell total population to a num
 
-            switch(array2[j][i]->GetPop())
+            switch(array2[j][i]->GetPop()){
 
             case 0:
             if (popnum >=1){
-                array2[j][i]->setPop(1)
+                array2[j][i]->SetPop(1);
             }
             break;
 
             case 1:
             if (popnum >=2){
-                array2[j][i]->setPop(2)
+                array2[j][i]->SetPop(2);
             }
             break;
 
             case 2:
             if (popnum >=8){
-                array2[j][i]->setPop(3)
+                array2[j][i]->SetPop(3);
             }
             break;
 
             case 3:
             if (popnum >=18){
-                array2[j][i]->setPop(4)
+                array2[j][i]->SetPop(4);
             }
             break;
 
             case 4:
             if (popnum >=32){
-                array2[j][i]->setPop(5)
+                array2[j][i]->SetPop(5);
             }
             break;            
 
@@ -50,4 +50,5 @@ void resUpdate(Node*** array1, Node*** array2, int arrayx, int arrayy){
 
 
 
+}
 }
