f9c9727561ade4783623d6026a78dcc3eb2293f8
Jones, Zachary <zacharyjones4@my.unt.edu>
2022-10-28 20:50:11 -0500

    Fixed compilation errors

--- checkAdjacent.cpp
+++ checkAdjacent.cpp
@@ -2,8 +2,9 @@
 
 bool isValidPos(int i, int j, int x, int y){
 
-    if (i < 0 || j < 0 || i > y - 1 || j > x - 1) //checks to see if selected cell in for loop is included in the bounds of the array
-        return false
+    if (i < 0 || j < 0 || i > y - 1 || j > x - 1){ //checks to see if selected cell in for loop is included in the bounds of the array
+        return false;
+    }
     return true;
 
 
@@ -20,43 +21,43 @@ int checkAdjacent(Node*** arr, int i, int j, int x, int y){
         }
     }
     if (isValidPos(i - 1, j, x, y)){
-        sum = sum + arr[j][i-1]->population;
+        sum = sum + arr[j][i-1]->GetPop();
         if (arr[j][i-1]->GetPop() == 0 && arr[j][i-1]->GetType() == 'T' || arr[j][i-1]->GetType() == '#' || arr[j][i-1]->GetType() == 'P' ){
             ++sum;
         }
     }
     if (isValidPos(i - 1, j + 1, x, y)){
-        sum = sum + arr[j+1][i-1]->population;
+        sum = sum + arr[j+1][i-1]->GetPop();
         if (arr[j+1][i-1]->GetPop() == 0 && arr[j+1][i-1]->GetType() == 'T' || arr[j+1][i-1]->GetType() == '#' || arr[j+1][i-1]->GetType() == 'P' ){
             ++sum;
         }
     }
     if (isValidPos(i, j - 1, x, y)){
-        sum = sum + arr[j-1][i]->population;
+        sum = sum + arr[j-1][i]->GetPop();
         if (arr[j-1][i]->GetPop() == 0 && arr[j-1][i]->GetType() == 'T' || arr[j-1][i]->GetType() == '#' || arr[j-1][i]->GetType() == 'P' ){
             ++sum;
         }    
     }
     if (isValidPos(i, j + 1, x, y)){
-        sum = sum + arr[j+1][i]->population;
+        sum = sum + arr[j+1][i]->GetPop();
         if (arr[j+1][i]->GetPop() == 0 && arr[j+1][i]->GetType() == 'T' || arr[j+1][i]->GetType() == '#' || arr[j+1][i]->GetType() == 'P' ){
             ++sum;
         }
     }
     if (isValidPos(i + 1, j - 1, x, y)){
-        sum = sum + arr[j-1][i+1]->population;
+        sum = sum + arr[j-1][i+1]->GetPop();
         if (arr[j-1][i+1]->GetPop() == 0 && arr[j-1][i+1]->GetType() == 'T' || arr[j-1][i+1]->GetType() == '#' || arr[j-1][i+1]->GetType() == 'P' ){
             ++sum;
         }
     }
     if (isValidPos(i + 1, j, x, y)){
-        sum = sum + arr[j][i+1]->population;
+        sum = sum + arr[j][i+1]->GetPop();
         if (arr[j][i+1]->GetPop() == 0 && arr[j][i+1]->GetType() == 'T' || arr[j][i+1]->GetType() == '#' || arr[j][i+1]->GetType() == 'P' ){
             ++sum;
         }
     }
     if (isValidPos(i + 1, j + 1, x, y)){
-        sum = sum + arr[j+1][i+1]->population;
+        sum = sum + arr[j+1][i+1]->GetPop();
         if (arr[j+1][i+1]->GetPop() == 0 && arr[j+1][i+1]->GetType() == 'T' || arr[j+1][i+1]->GetType() == '#' || arr[j+1][i+1]->GetType() == 'P' ){
             ++sum;
         }
