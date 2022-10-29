304ecf8b54df91704e935c899c82e3670ab19c72
Jones, Zachary <zacharyjones4@my.unt.edu>
2022-10-28 20:12:02 -0500

    Added functionality

--- checkAdjacent.h
+++ checkAdjacent.h
@@ -1,8 +1,11 @@
 #ifndef ADJACENT_H
 #define ADJACENT_H
 #include <iostream>
+#include "Node.h"
 
-int checkAdjacent();
+bool isValid(int i, int j, int x, int y);
+
+int checkAdjacent(Node*** arr, int i, int j, int x, int y);
 
 
 #endif
