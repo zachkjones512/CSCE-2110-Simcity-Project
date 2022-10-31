591de8cb47d0877eae88772a05977223e3208e79
Caleb Smay <calebsmay@my.unt.edu>
2022-10-31 18:11:28 -0500

    Added region analysis files

---
+++ regionAnalysis.cpp
@@ -0,0 +1,7 @@
+#include "regionAnalysis.h"
+
+void OutputPollution(int smallX, int smallY, int bigX, int bigY){}
+void PollutionTotal(int smallX, int smallY, int bigX, int bigY){}
+void OutputRegion(int smallX, int smallY, int bigX, int bigY){}
+void PopulationTotal(int smallX, int smallY, int bigX, int bigY){}
+bool BoundsValid(int smallX, int smallY, int bigX, int bigY){}
---
+++ regionAnalysis.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "node.h"
+
+void OutputPollution(int smallX, int smallY, int bigX, int bigY);
+void PollutionTotal(int smallX, int smallY, int bigX, int bigY);
+void OutputRegion(int smallX, int smallY, int bigX, int bigY);
+void PopulationTotal(int smallX, int smallY, int bigX, int bigY);
+bool BoundsValid(int smallX, int smallY, int bigX, int bigY);
