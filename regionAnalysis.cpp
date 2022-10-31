6d4359eec0f17f7b398ed9dda79bfa4847b5ae3d
Caleb Smay <calebsmay@my.unt.edu>
2022-10-31 18:32:52 -0500

    worked on regionAnalysis.*

--- regionAnalysis.cpp
+++ regionAnalysis.cpp
@@ -1,7 +1,20 @@
 #include "regionAnalysis.h"
 
-void OutputPollution(int smallX, int smallY, int bigX, int bigY){}
-void PollutionTotal(int smallX, int smallY, int bigX, int bigY){}
-void OutputRegion(int smallX, int smallY, int bigX, int bigY){}
-void PopulationTotal(int smallX, int smallY, int bigX, int bigY){}
-bool BoundsValid(int smallX, int smallY, int bigX, int bigY){}
+void OutputPollution(Node*** array, int smallX, int smallY, int bigX, int bigY){
+    for(int i = smallY; i < bigY; i++){        //Prints array
+        for(int j = smallX; j < bigX; j++){
+            Node* node = array[j][i];
+            if(node->GetPollution() > 0){
+                std::cout << node->GetPop();
+            }else{
+                std::cout << node->GetType();
+            }
+            std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+int PollutionTotal(Node*** array, int smallX, int smallY, int bigX, int bigY){}
+void OutputRegion(Node*** array, int smallX, int smallY, int bigX, int bigY){}
+int PopulationTotal(Node*** array, int smallX, int smallY, int bigX, int bigY){}
+bool BoundsValid(Node*** array, int smallX, int smallY, int bigX, int bigY){}
--- regionAnalysis.h
+++ regionAnalysis.h
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "node.h"
 
-void OutputPollution(int smallX, int smallY, int bigX, int bigY);
-void PollutionTotal(int smallX, int smallY, int bigX, int bigY);
-void OutputRegion(int smallX, int smallY, int bigX, int bigY);
-void PopulationTotal(int smallX, int smallY, int bigX, int bigY);
-bool BoundsValid(int smallX, int smallY, int bigX, int bigY);
+void OutputPollution(Node*** array, int smallX, int smallY, int bigX, int bigY);
+int PollutionTotal(Node*** array, int smallX, int smallY, int bigX, int bigY);
+void OutputRegion(Node*** array, int smallX, int smallY, int bigX, int bigY);
+int PopulationTotal(Node*** array, int smallX, int smallY, int bigX, int bigY);
+bool BoundsValid(Node*** array, int smallX, int smallY, int bigX, int bigY);
