2c56b2820bbf27e9566c9795f889c8e397cd6f41
Caleb Smay <calebsmay@my.unt.edu>
2022-10-31 22:12:05 -0500

    various post-simulation tools added

--- regionAnalysis.cpp
+++ regionAnalysis.cpp
@@ -5,6 +5,29 @@ void OutputPollution(Node*** array, int smallX, int smallY, int bigX, int bigY){
         for(int j = smallX; j < bigX; j++){
             Node* node = array[j][i];
             if(node->GetPollution() > 0){
+                std::cout << node->GetPollution();
+            }else{
+                std::cout << node->GetType();
+            }
+            std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+int PollutionTotal(Node*** array, int smallX, int smallY, int bigX, int bigY){  //Returns total pollution from desired region
+    int pollutionTotal = 0;
+    for(int i = smallY; i < bigY; i++){        
+        for(int j = smallX; j < bigX; j++){
+            Node* node = array[j][i];
+            pollutionTotal += node->GetPollution();
+        }
+    }
+}
+void OutputRegion(Node*** array, int smallX, int smallY, int bigX, int bigY){
+    for(int i = smallY; i < bigY; i++){        //Prints array
+        for(int j = smallX; j < bigX; j++){
+            Node* node = array[j][i];
+            if(node->GetPop() > 0){
                 std::cout << node->GetPop();
             }else{
                 std::cout << node->GetType();
@@ -14,7 +37,19 @@ void OutputPollution(Node*** array, int smallX, int smallY, int bigX, int bigY){
         std::cout << std::endl;
     }
 }
-int PollutionTotal(Node*** array, int smallX, int smallY, int bigX, int bigY){}
-void OutputRegion(Node*** array, int smallX, int smallY, int bigX, int bigY){}
-int PopulationTotal(Node*** array, int smallX, int smallY, int bigX, int bigY){}
-bool BoundsValid(Node*** array, int smallX, int smallY, int bigX, int bigY){}
+int PopulationTotal(Node*** array, int smallX, int smallY, int bigX, int bigY){ //Returns total population from desired region
+    int populationTotal = 0;
+    for(int i = smallY; i < bigY; i++){        
+        for(int j = smallX; j < bigX; j++){
+            Node* node = array[j][i];
+            populationTotal += node->GetPop();
+        }
+    }
+}
+bool BoundsValid(int arrayX, int arrayY, int smallX, int smallY, int bigX, int bigY){
+    if(smallX < 0 || smallY < 0 || bigX > arrayX || bigY > arrayY){
+        return false;
+    }else{
+        return true;
+    }
+}
--- regionAnalysis.h
+++ regionAnalysis.h
@@ -5,4 +5,4 @@ void OutputPollution(Node*** array, int smallX, int smallY, int bigX, int bigY);
 int PollutionTotal(Node*** array, int smallX, int smallY, int bigX, int bigY);
 void OutputRegion(Node*** array, int smallX, int smallY, int bigX, int bigY);
 int PopulationTotal(Node*** array, int smallX, int smallY, int bigX, int bigY);
-bool BoundsValid(Node*** array, int smallX, int smallY, int bigX, int bigY);
+bool BoundsValid(int arrayX, int arrayY, int smallX, int smallY, int bigX, int bigY);
