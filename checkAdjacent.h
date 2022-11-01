a7686964f4d50c1637b356b8c26994a85c57b343
Jones, Zachary <zacharyjones4@my.unt.edu>
2022-10-31 23:24:20 -0500

    Fixed funciton declaraction

--- checkAdjacent.h
+++ checkAdjacent.h
@@ -3,7 +3,7 @@
 #include <iostream>
 #include "Node.h"
 
-bool isValid(int i, int j, int x, int y);
+bool isValidPos(int i, int j, int x, int y);
 
 int checkAdjacent(Node*** arr, int i, int j, int x, int y);
 
