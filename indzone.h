6f7e79930c72f6548019bc4b7d2764de9db595e3
Jones, Zachary <zacharyjones4@my.unt.edu>
2022-11-02 11:50:35 -0500

    Fixed function declaration

--- indzone.h
+++ indzone.h
@@ -7,7 +7,7 @@
 // for more info, visit indzone.cpp
 
 void pollute(Node*** array2, int j, int i, int x, int y, int &pollution, int locPop);
-void indUpdate(Node*** array1, Node*** array2, int arrayX, int arrayY, int &workers, int &goods, bool &hasChanged);
+void indUpdate(Node*** array1, Node*** array2, int arrayX, int arrayY, int &workers, int &goods, int &pollution, bool &hasChanged);
 
 
 
